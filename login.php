00f1818656f9246da727f7631ac464c40c7d567a
SamaLin <105021058@live.asia.edu.tw>
2017-11-28 01:25:56 +0800

    1061128

---
+++ login.php
@@ -0,0 +1,53 @@
+<!-- 設定網頁編碼為UTF-8 -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!--<link rel="stylesheet" href="Tech/lg.css">-->
+<!--<link href='http://fonts.googleapis.com/css?family=Raleway:500' rel='stylesheet' type='text/css'>-->
+<form name="form" method="post" action="connect.php">
+    <!--    <div class="middlePage">-->
+    <!--        <div class="page-header">-->
+    <!--            <h1 class="logo">Techulus <small>Welcome to our place!</small></h1>-->
+    <!--        </div>-->
+    <!---->
+    <!--        <div class="panel panel-info">-->
+    <!--            <div class="panel-heading">-->
+    <!--                <h3 class="panel-title">Please Sign In</h3>-->
+    <!--            </div>-->
+    <!--            <div class="panel-body">-->
+    <!---->
+    <!--                <div class="row">-->
+    <!---->
+    <!--                    <div class="col-md-5" >-->
+    <!--                        <a href="#"><img src="http://techulus.com/buttons/fb.png" /></a><br/>-->
+    <!--                        <a href="#"><img src="http://techulus.com/buttons/tw.png" /></a><br/>-->
+    <!--                        <a href="#"><img src="http://techulus.com/buttons/gplus.png" /></a>-->
+    <!--                    </div>-->
+    <!---->
+    <!--                    <div class="col-md-7" style="border-left:1px solid #ccc;height:160px">-->
+    <!--                        <form class="form-horizontal">-->
+    <!--                            <fieldset>-->
+    <!---->
+    <!--                                <input id="textinput" name="id" type="text" placeholder="Enter User Name" class="form-control input-md">-->
+    <!--                                <div class="spacing"><input type="checkbox" name="checkboxes" id="checkboxes-0" value="1"><small> Remember me</small></div>-->
+    <!--                                <input id="textinput" name="pw" type="password" placeholder="Enter Password" class="form-control input-md">-->
+    <!--                                <div class="spacing"><a href="#"><small> Forgot Password?</small></a><br/></div>-->
+    <!--                                <button id="singlebutton" type="submit" name="button" value="登入" class="btn btn-info btn-sm pull-right">Sign In</button>-->
+    <!---->
+    <!---->
+    <!--                            </fieldset>-->
+    <!--                        </form>-->
+    <!--                    </div>-->
+    <!---->
+    <!--                </div>-->
+    <!---->
+    <!--            </div>-->
+    <!--        </div>-->
+    <!---->
+    <!--        <p><a href="https://github.com/arjunkomath">About</a> · Arjun</p>-->
+    <!---->
+    <!--    </div>-->
+    帳號：<input  type="text" name="id" /> <br>
+    密碼：<input  type="password" name="pw" /> <br>
+    <input type="submit" name="button" value="登入" />&nbsp;&nbsp;
+
+    <a href="register.php">申請帳號</a>
+</form>
---
+++ mysql_connect.inc.php
@@ -0,0 +1,25 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php
+//資料庫設定
+//資料庫位置
+$db_server = "localhost";
+//資料庫名稱
+$db_name = "105021059";
+//資料庫管理者帳號
+$db_user = "105021059";
+//資料庫管理者密碼
+$db_password = "#a7a4YXPM";
+//對資料庫連線
+
+$connect = @mysqli_connect($db_server, $db_user, $db_password,$db_name);
+if(!$connect)
+    die("無法對資料庫連線");
+//資料庫連線採UTF8
+
+mysqli_query("SET NAMES utf8");
+
+//選擇資料庫
+//if(!@mysqli_select_db($db_name))
+//    die("無法使用資料庫");
+
+?>
